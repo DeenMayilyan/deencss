324cf8d4ef94bb839496bf5d7fbdb2ef7e6ed04b
DeenMayilyan <dinmayilyan37@gmail.com>
2025-02-23 19:20:21 +0100

    firs commit

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import './deen.css'
+
+const App = () => {
+    return (
+
+       <table className="table" align="center">
+           <tr>
+               <td>
+                   <table>
+                       <tr>
+                           <td className="td1">
+                               <p>
+                                   Its a long established fact that a reader will be distributed by readable content
+                                   of a page when looking at its layoot. The point of using Lorem Ipsum is that it has
+                                   a mare-or-less normal distribation of letters, as opposed to using` Content here,
+                                   content here, making it look like readable English Many desktop The point of using
+                                   Lorem Ipsum is that it has a mare-or-less normal distribation of letters, as opposed
+                                   to using` Content here, making it look like readable English Many desktop.
+                               </p>
+                           </td>
+                       </tr>
+                   </table>
+               </td>
+           </tr>
+           <tr>
+               <td>
+                   <table align={"center"}>
+                       <tr>
+                           <td className="td2">
+                               <h2 align={"center"}>
+                                   Lorem Lorem!!!!
+                               </h2>
+                               <p>
+                                   Its a long established fact that a reader will be distributed by readable content
+                                   of a page when looking at its layoot. The point of using Lorem Ipsum is that it has
+                                   a mare-or-less normal distribation of letters, as opposed to using` Content here,
+                                   content here, making it look like readable English Many desktop The point of using
+                                   Lorem Ipsum is that it has a mare-or-less normal distribation of letters, as opposed
+                                   to using` Content here, making it look like readable English Many desktop.making it
+                                   look like readable English Many desktop readable English Many desktop. making it.
+                               </p>
+                           </td>
+                       </tr>
+                   </table>
+               </td>
+           </tr>
+           <tr>
+               <td>
+                   <table align={"right"}>
+                       <tr>
+                           <td className="td3">
+                               <h1 align={"center"}>
+                                  Lorem Lorem!!!!
+                               </h1>
+                           </td>
+                       </tr>
+                   </table>
+               </td>
+           </tr>
+       </table>
+    );
+};
+
+export default App;
