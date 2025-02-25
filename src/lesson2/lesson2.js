90995b09758be2c0f7462a852f1fcf8faeee7f6f
DeenMayilyan <dinmayilyan37@gmail.com>
2025-02-25 22:18:09 +0100

    lesson2

--- src/lesson2/lesson2.js
+++ src/lesson2/lesson2.js
@@ -1,10 +1,167 @@
 import React from 'react';
-
+import './lesson2.css'
 const Lesson2 = () => {
     return (
-        <div>
-            lesson 2
-        </div>
+        <table className="aesthetic">
+            <tr>
+                <td>
+                    <table className={"sunset"} align={"center"}>
+                        <tr>
+                            <td>
+                                <p className={"p"}>
+                                    My Day
+                                </p>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <table className={"table"} align={"center"}>
+                        <tr>
+                            <td>
+                                <table>
+                                    <tr>
+                                        <td>
+                                            <a href={"https://youtu.be/NpC643b7-bE?si=8q8-nnqD53kp7CgE"} target="_blank"
+                                            className={"link"}>
+                                                Happy Day
+                                            </a>
+                                        </td>
+                                        <td>
+                                            <a href={"https://youtu.be/IuFEFPBq4As?si=gyEE0OzHHKuF6OIU"} target="_blank"
+                                            className={"link"}>
+                                                Happy Life
+                                           </a>
+                                        </td>
+                                        <td>
+                                            <a href={"https://youtu.be/3_JwkSBzZqM?si=jA_og156V0ze1Xbi"} target="_blank"
+                                            className={"link"}>
+                                                Be Happy
+                                            </a>
+                                        </td>
+                                        <td>
+                                            <a href={"https://youtu.be/9oRhKxxDHIg?si=03oIh3SF-nQ3A7Er"} target="_blank"
+                                            className={"link"}>
+                                                Dont Worry
+                                            </a>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <table className={"table1"} align={"center"}>
+                        <tr>
+                            <td>
+                                <h1 className={"h1"}>
+                                    SIMPLE DAY
+                                </h1>
+                                <p className={"link3"}>
+                                    <a href={"https://youtu.be/P0nER6gMB3g?si=lWYMbWZLdaMQIB84"} target={"_blank"}
+                                    className={"link1"}>
+                                        Dont Worry, Be Happy
+                                    </a>
+                                </p>
+                                <p>
+                                    It is a long established fact that a reader will be distracted by the readable content
+                                    of a page when looking at its layout. The point of using Lorem Ipsum is that it has a
+                                    more-or-less normal distribution of letters, as opposed to using 'Content here, content
+                                    here', making it look like readable English. Many desktop publishing packages and web
+                                    page editors now use Lorem Ipsum as their default model text, and a search for 'lorem
+                                    ipsum' will uncover many web sites still in their infancy.
+                                </p>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <table className={"table2"} align={"center"}>
+                        <tr>
+                            <td>
+                                <h3>
+                                    Happy Day
+                                </h3>
+                                <p className={"link3"}>
+                                    <a href={"#ffdf82"} target={"_blank"} className={"link2"}>
+                                        September 2013
+                                    </a>
+                                </p>
+                                <p>
+                                    It is a long established fact that a reader will be distracted by the readable content
+                                    of a page when looking at its layout. The point of using Lorem Ipsum is that it has a
+                                    more-or-less normal distribution of letters, as opposed to using 'Content here, content
+                                    here', making it look like readable English. Many desktop publishing packages and web
+                                    page editors now use Lorem Ipsum as their default model text, and a search for 'lorem
+                                    ipsum' will uncover many web sites still in their infancy.
+                                </p>
+                            </td>
+                            <td>
+                                <h3>
+                                    Happy life
+                                </h3>
+                                <p className={"link3"}>
+                                    <a href={"#ffdf82"} target={"_blank"} className={"link2"}>
+                                        September 2013
+                                    </a>
+                                </p>
+                                <p>
+                                    It is a long established fact that a reader will be distracted by the readable content
+                                    of a page when looking at its layout. The point of using Lorem Ipsum is that it has a
+                                    more-or-less normal distribution of letters, as opposed to using 'Content here, content
+                                    here', making it look like readable English. Many desktop publishing packages and web
+                                    page editors now use Lorem Ipsum as their default model text, and a search for 'lorem
+                                    ipsum' will uncover many web sites still in their infancy.
+                                </p>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <table cellPadding={"5px"}>
+                        <tr>
+                            <td>
+                                <p>
+                                    <a href={"#ffdf82"} target={"_blank"}>
+                                        Happy Day
+                                    </a>
+                                </p>
+                            </td>
+                            <td>
+                                <p>
+                                    <a href={"#ffdf82"} target={"_blank"}>
+                                        Happy Life
+                                    </a>
+                                </p>
+                            </td>
+                            <td>
+                                <p>
+                                    <a href={"#ffdf82"} target={"_blank"}>
+                                        Be Happy
+                                    </a>
+                                </p>
+                            </td>
+                            <td>
+                                <p>
+                                    <a href={"#ffdf82"} target={"_blank"}>
+                                        Dont Worry
+                                    </a>
+                                </p>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
     );
 };
 
