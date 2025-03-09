545009bf453ff183fdaa62151a32835b1a249bc2
DeenMayilyan <dinmayilyan37@gmail.com>
2025-03-09 22:31:06 +0100

    lesson12

---
+++ src/lesson13/lesson13.js
@@ -0,0 +1,166 @@
+import React from 'react';
+// import classes from './lesson13.module.css'
+const Lesson13 = () => {
+    return (
+        <div>
+            <header>
+                <div className={classes.home}>
+                    <a>
+                        HOME
+                    </a>
+                </div>
+                <div className={classes.home}>
+                    <a>
+                        USER LOGIN
+                    </a>
+                </div>
+            </header>
+            <div className={classes.div}>
+                <div className={classes.joy}>
+                    TravelJoy
+                </div>
+                <div className={classes.clasik}>
+                    <div className={classes.divik}>
+                        HOME
+                    </div>
+                    <div className={classes.divik}>
+                        ABOUT
+                    </div>
+                    <div className={classes.divik}>
+                        GALLERY
+                    </div>
+                    <div className={classes.divik}>
+                        BLOG
+                    </div>
+                    <div className={classes.navik}>
+                        <div className={classes.divikk}>
+                            CONTACTS
+                        </div>
+                        <div className={classes.klorner}>
+                            <div className={classes.f}>
+
+                            </div>
+                            <div className={classes.i}>
+
+                            </div>
+                            <div className={classes.w }>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={classes.kaqav}>
+                    <div className={classes.soxak}>
+                        <div className={classes.relax}>
+
+                        </div>
+                        <div className={classes.gap}>
+                            <div className={classes.water}>
+                                BLUE WATERS RESORT
+                            </div>
+                            <div className={classes.havik}>
+                                <p>
+                                    Lorem Ipsum is dummy text of the printing and typesetting industry.
+                                    Lorem Ipsum is dummy text of the printing and typesetting industry.
+                                    Lorem Ipsum is dummy text of the printing and typesetting industry.
+                                </p>
+                            </div>
+                            <div className={classes.d}>
+                                <div className={classes.gin}>
+                                    $334
+                                </div>
+                                <div>
+                                    <input type={"button"} value={"More"}/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={classes.soxak}>
+                        <div className={classes.relax2}>
+
+                        </div>
+                        <div className={classes.gap}>
+                            <div className={classes.water}>
+                                WINE COUNTRY ESCAPES
+                            </div>
+                            <div className={classes.havik}>
+                                <p>
+                                    Lorem Ipsum is dummy text of the printing and typesetting industry.
+                                    Lorem Ipsum is dummy text of the printing and typesetting industry.
+                                    Lorem Ipsum is dummy text of the printing and typesetting industry.
+                                </p>
+                            </div>
+                            <div className={classes.d}>
+                                <div className={classes.gin}>
+                                    $975
+                                </div>
+                                <div>
+                                    <input type={"button"} value={"More"}/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={classes.soxak}>
+                        <div className={classes.relax3}>
+
+                        </div>
+                        <div className={classes.gap}>
+                            <div className={classes.waterr}>
+                                HOTEL MAKANDA BY THE SEA
+                            </div>
+                            <div className={classes.havik}>
+                                <p>
+                                    Lorem Ipsum is dummy text of the printing and typesetting industry.
+                                    Lorem Ipsum is dummy text of the printing and typesetting industry.
+                                    Lorem Ipsum is dummy text of the printing and typesetting industry.
+                                </p>
+                            </div>
+                            <div className={classes.d}>
+                                <div className={classes.gin}>
+                                    $789
+                                </div>
+                                <div>
+                                    <input type={"button"} value={"More"}/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={classes.soxak}>
+                        <div className={classes.relax4}>
+
+                        </div>
+                        <div className={classes.gap}>
+                            <div className={classes.water}>
+                                DESTINATIONS
+                            </div>
+                            <div className={classes.havik}>
+                                <p>
+                                    Lorem Ipsum is dummy text of the printing and typesetting industry.
+                                    Lorem Ipsum is dummy text of the printing and typesetting industry.
+                                    Lorem Ipsum is dummy text of the printing and typesetting industry.
+                                </p>
+                            </div>
+                            <div className={classes.d}>
+                                <div className={classes.gin}>
+                                    $734
+                                </div>
+                                <div>
+                                    <input type={"button"} value={"More"}/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <footer>
+                <div className={classes.home}>
+                    <a>
+                        TRAVEL JOY (C) 2013 | PRIVACY POLICY
+                    </a>
+                </div>
+            </footer>
+        </div>
+    );
+};
+
+export default Lesson13;
